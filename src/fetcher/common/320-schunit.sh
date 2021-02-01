0121dc65f84e1061d6d539c94fc4b7ddb51b3ea6
Erlend Klakegg Bergheim <erlend@klakegg.net>
2021-02-01 14:33:48 +0000

    Bumping version of SchUnit.

--- src/fetcher/common/320-schunit.sh
+++ src/fetcher/common/320-schunit.sh
@@ -3,14 +3,14 @@
 set -e
 set -u
 
-VERSION=${SCHUNIT_VERSION:-"0.1.0"}
+VERSION=${SCHUNIT_VERSION:-"0.2.0"}
 
 # Download
-download schunit-core-${VERSION}-dist.tar.gz \
-  https://github.com/schunit/schunit/releases/download/v${VERSION}/schunit-core-${VERSION}-dist.tar.gz
+download schunit-cli-${VERSION}-dist.tar.gz \
+  https://github.com/schunit/schunit/releases/download/v${VERSION}/schunit-cli-${VERSION}-dist.tar.gz
 
 # Prepare folder
 mkdir -p $DIST/lib/schunit
 
 # Unzip content
-tar xzf $TMP/schunit-core-${VERSION}-dist.tar.gz -C $DIST/lib/schunit
+tar xzf $TMP/schunit-cli-${VERSION}-dist.tar.gz -C $DIST/lib/schunit
