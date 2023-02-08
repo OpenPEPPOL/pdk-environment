2e5fa2af6d0a1375004ba216afa2e9238471d50d
Jerry Dimitriou <jd@inteleproc.eu>
2023-02-08 22:21:59 +0200

    updated windows hugo script

--- src/fetcher/win-amd64/600-hugo.sh
+++ src/fetcher/win-amd64/600-hugo.sh
@@ -10,6 +10,6 @@ VERSION=${HUGO_VERSION}
 mkdir -p $DIST/lib/hugo
 
 download hugo_${VERSION}_Windows-64bit.zip \
-  https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Windows-64bit.zip
+  https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_windows-amd64.zip
 
 unzip -qo $TMP/hugo_${VERSION}_Windows-64bit.zip -d $DIST/lib/hugo
